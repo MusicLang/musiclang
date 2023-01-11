4a3b6b285b626f82b139f5b7d6cc440f32f86eb7
Florian GARDIN <fgardin.pro@gmail.com>
2023-01-11 16:32:01 +0000

    Bump version: 0.5.3 → 0.5.4 [skip ci]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="musiclang",
-    version="0.5.3",
+    version="0.5.4",
     author="Florian GARDIN",
     author_email="fgardin.pro@gmail.com",
     description=("A python package for music notation and generation"
@@ -50,7 +50,7 @@ setuptools.setup(
         "urllib3==1.26.13",
         "wcwidth==0.2.5",
         "webcolors==1.12",
-        "webencodings==0.5.3",
+        "webencodings==0.5.4",
         "websocket-client==1.4.2",
         "Werkzeug==2.2.2",
         "widgetsnbextension==4.0.3",
@@ -58,7 +58,7 @@ setuptools.setup(
         "xmlschema==2.1.1",
         "zipp==3.11.0",
         "torch==1.13.0",
-        "torchdata==0.5.3",
+        "torchdata==0.5.4",
         "torchtext==0.14.0"
                       ],
     packages=setuptools.find_packages(include='*'),
